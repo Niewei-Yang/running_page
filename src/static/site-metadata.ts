7e4dee958a81fce637cd3bd5514bb6a65b46ccc9
Niewei <138655239+Niewei-Yang@users.noreply.github.com>
2026-01-12 21:13:13 +0800

    Update site-metadata.ts
    更改标题为Sports Map

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,7 +15,7 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Neewii的Running Page',
+  siteTitle: 'Neewii的Sports Map',
   siteUrl: 'https://neewii-running-page.vercel.app/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
